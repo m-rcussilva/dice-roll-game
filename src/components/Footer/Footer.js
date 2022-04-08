3ae97fe468c06f6d882d4b8228edcab61c338287
marcusluis <marcusluis.mail@gmail.com>
2022-04-08 19:11:26 -0300

    adicionando link no footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ class Footer extends Component {
     render() {
         return (
             <div className="container-footer">
-                <a className="github-link" href="">
+                <a className="github-link" href="https://github.com/m-rcussilva/dice-roll-game" target="_blank">
                     Github
                 </a>
             </div>
